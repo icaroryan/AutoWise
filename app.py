ec357b27e7604160235f0f9bfb82ba1a02cfa52c
Icaro Ryan <rfvb10hu@gmail.com>
2020-09-19 12:14:35 -0300

    Adding forex-python api to query the exchange rate

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 import os, re
+from time import sleep
 
 # https://github.com/RomelTorres/alpha_vantage
-from alpha_vantage.foreignexchange import ForeignExchange
+from forex_python.converter import CurrencyRates
 
 # Loading Environment Variables
 from dotenv import load_dotenv
 load_dotenv()
 
+# Tracker (GET_EXCHANGE_RATES) -> Create a function that gets the chosen exchange rates
+def get_exchange_rates(from_currency, to_currency):
+    c = CurrencyRates()
+    data = c.get_rate(from_currency, to_currency)
+    print(f"{data}")
+    sleep(2)
+
+
 # Alphavantage API_KEY
 try:
-    api = os.environ.get('API_KEY')
+    api_key = os.environ.get('API_KEY')
 except KeyError:
     print("API Key not found!")
 
@@ -19,9 +28,11 @@ to_currency = input(f"\nWhat Currency you want to convert TO? (ex: CAD) ")
 
 # Ask if the user wants the automatic money sender turned ON (transfer wise)
 auto_prompt = input(f"\nDo you want us to automatically open a transaction when the Exchange Rate reach a threshold? ([Y]es or [N]o) ")
-
 auto_mode = None
 # auto_mode = re.??? ### TO-DO
+
+while True:
+    get_exchange_rates(from_currency, to_currency)
 
 if auto_mode:
     pass
@@ -29,9 +40,7 @@ if auto_mode:
     # Prompt for the transferwise password (don't save this in a raw form, rather, encrypted way)
     
 
-# Tracker (GET_EXCHANGE_RATES) -> Create a function that gets the chosen exchange rates
-def get_exchange_rates(from_currency, to_currency):
-    pass
+
 # INPUT: from_currency(str), to_currency(str)
 # OUTPUT: rate (Float number, rounded by 4)
 
