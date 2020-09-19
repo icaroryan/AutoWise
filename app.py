104315ab15a3cc7b96f891caf6c8ececb7f30ca8
Icaro Ryan <rfvb10hu@gmail.com>
2020-09-19 16:49:17 -0300

    Tweaking the code and adding error handling

--- app.py
+++ app.py
@@ -1,30 +1,23 @@
 import os, re
 from time import sleep
+from datetime import datetime
 
 # https://github.com/RomelTorres/alpha_vantage
-from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyRates, CurrencyCodes
 
-# Loading Environment Variables
-from dotenv import load_dotenv
-load_dotenv()
+cr = CurrencyRates()
+cc = CurrencyCodes()
 
 # Tracker (GET_EXCHANGE_RATES) -> Create a function that gets the chosen exchange rates
-def get_exchange_rates(from_currency, to_currency):
-    c = CurrencyRates()
-    data = c.get_rate(from_currency, to_currency)
-    print(f"{data}")
-    sleep(2)
+def get_exchange_rates(from_currency, to_currency, now):
+    data = cr.get_rate(from_currency, to_currency, now)
+    return data
 
-
-# Alphavantage API_KEY
-try:
-    api_key = os.environ.get('API_KEY')
-except KeyError:
-    print("API Key not found!")
 
 # Prompt the user for the Currencies
 from_currency = input(f"\nWhat Currency you want to convert FROM? (ex: USD) ")
-to_currency = input(f"\nWhat Currency you want to convert TO? (ex: CAD) ")
+
+to_currency = input(f"What Currency you want to convert TO? (ex: CAD) ")
 
 # Ask if the user wants the automatic money sender turned ON (transfer wise)
 auto_prompt = input(f"\nDo you want us to automatically open a transaction when the Exchange Rate reach a threshold? ([Y]es or [N]o) ")
@@ -32,7 +25,18 @@ auto_mode = None
 # auto_mode = re.??? ### TO-DO
 
 while True:
-    get_exchange_rates(from_currency, to_currency)
+    from_currency = from_currency.upper()
+    to_currency = to_currency.upper()
+    now = datetime.now()
+
+    try:
+        rate = get_exchange_rates(from_currency, to_currency, now)
+    except:
+        print("\n Currency not found, please try again!")
+        break
+
+    print(f"1 {from_currency} = {round(rate, 4)} {to_currency}")
+    sleep(1)
 
 if auto_mode:
     pass
